c819a97d7f3f1ea18aaa205a7aede9d5b7819372
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-05-31 13:54:07 +0200

    Create a Builder for OpenAPIExporter

---
+++ plugins/edu.uoc.som.openapi.io/src/edu/uoc/som/openapi/io/ExporterBuilder.java
@@ -0,0 +1,12 @@
+package edu.uoc.som.openapi.io;
+
+public class ExporterBuilder {
+  
+	public static OpenAPIExporter create() {
+		
+		
+		OpenAPIExporter openAPIExporter = new OpenAPIExporter();
+		
+		return openAPIExporter;
+	}
+}
