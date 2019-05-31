832d20882946da62a87e4b704f8bf57f27f17ad0
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-05-31 12:24:48 +0200

    Create a method to save the json definition

--- plugins/edu.uoc.som.openapi.io/src/edu/uoc/som/openapi/io/utils/IOUtils.java
+++ plugins/edu.uoc.som.openapi.io/src/edu/uoc/som/openapi/io/utils/IOUtils.java
@@ -1,5 +1,23 @@
 package edu.uoc.som.openapi.io.utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 public class IOUtils {
 
+	public static void saveOpenAPIDefintion(JsonObject jsonDefinition, File output) throws IOException {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String jsonOutput = gson.toJson(jsonDefinition); 
+		FileWriter fileWritter = new FileWriter(output.getPath(), true);
+		BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+		bufferWritter.write(jsonOutput);
+		bufferWritter.close();
+		
+	}
 }
