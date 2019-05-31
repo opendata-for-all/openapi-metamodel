362578266a7e60f64cb1c37c89bd335cd41e7ce2
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-05-31 12:10:24 +0200

    Move OpenAPIUtils to edu.uoc.som.openpai.util

---
+++ plugins/edu.uoc.som.openapi/src/edu/uoc/som/openapi/util/OpenAPIUtils.java
@@ -0,0 +1,15 @@
+package edu.uoc.som.openapi.util;
+
+import edu.uoc.som.openapi.API;
+import edu.uoc.som.openapi.Definition;
+import edu.uoc.som.openapi.Schema;
+
+public class OpenAPIUtils {
+	public static Definition getDefinitionFromSchema(API api, Schema schema) {
+		for(Definition definition: api.getDefinitions()) {
+			if(definition.getSchema().equals(schema))
+				return definition;
+		}
+		return null;
+	}
+}
