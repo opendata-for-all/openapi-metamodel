a5e35d518565568e52138db18d775a13ecadf60b
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-08-05 16:41:46 +0200

    Update the builder

--- openapi2/plugins/edu.uoc.som.openapi2.io/src/edu/uoc/som/openapi2/io/OpenAPI2Builder.java
+++ openapi2/plugins/edu.uoc.som.openapi2.io/src/edu/uoc/som/openapi2/io/OpenAPI2Builder.java
@@ -2,7 +2,11 @@ package edu.uoc.som.openapi2.io;
 
 import java.io.File;
 import java.io.IOException;
+
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+
 import edu.uoc.som.openapi2.API;
+import edu.uoc.som.openapi2.io.exception.OpenAPIValidationException;
 import edu.uoc.som.openapi2.io.model.SerializationFormat;
 
 public class OpenAPI2Builder {
@@ -21,13 +25,13 @@ public class OpenAPI2Builder {
 		return this;
 	}
 	
-	public API fromFile(File file) throws IOException {
+	public API fromFile(File file) throws IOException, ProcessingException, OpenAPIValidationException {
 		OpenAPI2Importer importer = new OpenAPI2Importer();
 		return importer.createOpenAPI2ModelFromFile(file, serializationFormat);
 		
 	}
 
-	public API fromText(String text) {
+	public API fromText(String text) throws ProcessingException, IOException, OpenAPIValidationException {
 		OpenAPI2Importer importer = new OpenAPI2Importer();
 		return importer.createOpenAPI2ModelFromText(text, serializationFormat);
 	}
