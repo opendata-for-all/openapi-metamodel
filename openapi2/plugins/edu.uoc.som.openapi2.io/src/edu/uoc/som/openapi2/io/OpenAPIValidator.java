f24cc30373dbb95b7914e0662a9d7bd4048815e5
Hamza Ed-doubi <hed-douibi@uoc.edu>
2019-08-05 16:41:35 +0200

    Validation support implementation

---
+++ openapi2/plugins/edu.uoc.som.openapi2.io/src/edu/uoc/som/openapi2/io/OpenAPIValidator.java
@@ -0,0 +1,34 @@
+package edu.uoc.som.openapi2.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+
+import edu.uoc.som.openapi2.io.model.OpenAPIValidationReport;
+
+public class OpenAPIValidator {
+	JsonSchema openAPIschema = null;
+
+	public OpenAPIValidator() throws IOException, ProcessingException {
+		JsonNode openAPIJson = null;
+		InputStream stream = this.getClass().getResourceAsStream("/schema.json");
+		openAPIJson = JsonLoader.fromReader(new InputStreamReader(stream));
+		JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+		openAPIschema = factory.getJsonSchema(openAPIJson);
+	}
+
+	public OpenAPIValidationReport validate(File openAPIDef) throws ProcessingException, IOException {
+		return new OpenAPIValidationReport(openAPIschema.validate(JsonLoader.fromFile(openAPIDef)));
+	}
+
+	public OpenAPIValidationReport validate(String openAPIDef) throws ProcessingException, IOException {
+		return new OpenAPIValidationReport(openAPIschema.validate(JsonLoader.fromString(openAPIDef)));
+	}
+}
---
+++ openapi2/plugins/edu.uoc.som.openapi2.io/src/edu/uoc/som/openapi2/io/exception/OpenAPIValidationException.java
@@ -0,0 +1,16 @@
+package edu.uoc.som.openapi2.io.exception;
+
+public class OpenAPIValidationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OpenAPIValidationException(String message) {
+		super(message);
+		
+	}
+
+	
+}
